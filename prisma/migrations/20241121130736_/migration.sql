5c2c1d1e1453403bfbd8502f1f112ca648cce77f
a-s-t-e-y-a <krishnoit100@gmail.com>
2024-11-21 18:38:17 +0530

    REFACTOR : remove type od issues

---
+++ prisma/migrations/20241121130736_/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `type_issues` on the `githubUrl` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "githubUrl" DROP COLUMN "type_issues";
